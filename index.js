a317e11d085bef55a5bca47ab8deafd50593997b
Maurice Prosper <maurice.prosper@ttu.edu>
2016-04-01 03:25:49 -0500

    Builder now creates environment and draws svg on their own Completely reworked for the builder

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const FILE_IN = __dirname + '/assets/MachineLearning.txt'; // raw input
-const FILE_OUT = __dirname + '/assets/data.json'; // parsed output
 
 const fs = require('fs');
 const parser = require('./lib/parser');
 const builder = require('./lib/builder');
 
 fs.readFile(FILE_IN, 'utf8', function (err, data) {
-    if(err)
-        console.error(err);
-
-    try {
-        ret = parser(data);
-        chart = builder(ret);
-    } catch(err) {
-        console.error('Unable to parse data');
-    }
+    if(err) console.error(err);
+    else builder({
+            path: __dirname + '/node_modules/d3/d3.js',
+            data: parser(data),
+            done: function(err, data) {
+                if(err) console.error('Can not build svg', err);
+                else console.log(data);
+            }
+        });
 });
