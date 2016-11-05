6d15b1cae3a294be9331c46347b027fb030c13ee
Maurice Prosper <maurice.prosper@ttu.edu>
2016-11-05 15:59:20 -0700

    graph building and some regex!

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const FILE_IN = __dirname + '/assets/MachineLearning.txt'; // raw input
+// const FILE_IN = __dirname + '/assets/train1.log'; // raw input
 
 const fs = require('fs');
 const parser = require('./lib/parser');
 const builder = require('./lib/builder');
 
+/*
 fs.readFile(FILE_IN, 'utf8', function (err, data) {
     if(err) console.error(err);
     else builder({
@@ -14,4 +15,21 @@ fs.readFile(FILE_IN, 'utf8', function (err, data) {
                 else console.log(data);
             }
         });
+});*/
+const path = 'C:\\Users\\Mo\\Desktop\\logs\\';
+fs.readdir(path, function(err, files) {
+    if(err) throw err;
+    files.forEach(function (file) {
+        fs.readFile(path + file, 'utf8', function (err, data) {
+            if(err) console.error(err);
+            else builder({
+                path: __dirname + '/node_modules/d3/d3.js',
+                data: parser(data),
+                done: function(err, data) {
+                    if(err) console.error('Can not build svg', err);
+                    else fs.writeFileSync(path +'svg\\'+ file + '.svg', data)
+                }
+            });
+        });
+    })
 });
